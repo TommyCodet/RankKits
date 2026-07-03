877efb1114168cf8f81eae8a971269b0a02e46d3
TommyCodet <tommycodet247@gmail.com>
2026-07-03 07:36:08 +0200

    Create RanksCommand.java

---
+++ src/main/java/de/meinserver/rankkits/commands/RanksCommand.java
@@ -0,0 +1,29 @@
+package de.meinserver.rankkits.commands;
+
+import de.meinserver.rankkits.gui.RankGUI;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class RanksCommand implements CommandExecutor {
+
+    public RanksCommand(Object plugin) {
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender,
+                             Command command,
+                             String label,
+                             String[] args) {
+
+        if (!(sender instanceof Player player)) {
+            sender.sendMessage("Nur Spieler können diesen Befehl nutzen.");
+            return true;
+        }
+
+        RankGUI.open(player);
+
+        return true;
+    }
+}
